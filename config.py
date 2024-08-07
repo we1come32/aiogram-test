c01ba5e5964163b027ac93e26b293b19d874d77b
Ilya Kanyshev <i.kanyshev@inform.gazprom.ru>
2024-08-07 09:17:27 +0300

    Создана база для бота

---
+++ config.py
@@ -0,0 +1,11 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = getenv('TELEGRAM_BOT_TOKEN', '')
+assert BOT_TOKEN
+
+# Only for redis settings
+CACHE_STORAGE = getenv('CACHE_STORAGE', '')
---
+++ main.py
@@ -0,0 +1,27 @@
+import importlib
+
+from aiogram import Dispatcher, Bot
+from loguru import logger
+
+import config
+
+if config.CACHE_STORAGE:
+    from aiogram.fsm.storage.redis import RedisStorage
+    storage = RedisStorage(config.CACHE_STORAGE)
+else:
+    from aiogram.fsm.storage.memory import MemoryStorage
+    storage = MemoryStorage()
+
+dp = Dispatcher(storage=storage)
+
+
+if __name__ == '__main__':
+    bot = Bot(config.BOT_TOKEN)
+    try:
+        for i in range(1, 100):
+            module = importlib.import_module(f'tasks.number_{i}')
+            dp.include_router(module.router)
+            logger.info("Imported tasks.number_{number}", number=i)
+    except ImportError:
+        pass
+    dp.run_polling(bot)
